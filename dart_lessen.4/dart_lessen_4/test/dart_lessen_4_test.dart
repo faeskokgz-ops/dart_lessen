f308fa4739fd9399dae50cffc8c68af56230f347
faeskokgz-ops <faeskokgz@gmail.com>
2026-06-14 23:13:34 +0600

    added commet

--- dart_lessen.4/dart_lessen_4/test/dart_lessen_4_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_lessen_4/dart_lessen_4.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- dart_lessen_1/bin/dart_lessen.4.dart
+++ dart_lessen_1/bin/dart_lessen.4.dart
@@ -1,3 +1,4 @@
+// hi sensei
 // Глобальная переменная
 int totalCalls = 0;
 
